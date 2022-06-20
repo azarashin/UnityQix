d86d35a8a1c87ca98789b30b48b803f11a6fc222
azarashin <azarashin1979@gmail.com>
2022-06-21 04:12:22 +0900

    ファイルを移動。

--- Assets/UnityQix/Scripts/Core/Factory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Factory : MonoBehaviour
-{
-    public abstract AreaCalculator GenerateAreaCalculator();
-
-}
