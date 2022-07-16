21c46d1da31bfbdbe838b78878d20e87029ef0e1
azarashin <azarashin1979@gmail.com>
2022-07-17 00:25:56 +0900

    プレイヤーのライフと残機の概念を追加(このファイルの扱いは別途見直す必要あり)。

--- Assets/UnityQix/Scripts/Core/GameBoard.cs
+++ Assets/UnityQix/Scripts/Core/GameBoard.cs
@@ -13,16 +13,25 @@ public abstract class GameBoard : MonoBehaviour
     [SerializeField]
     int _stage = 0;
 
+    [Range(1, 16)]
+    [SerializeField]
+    int _lives = 1;
+
+    [Range(1, 16)]
+    [SerializeField]
+    int _units = 1; 
+
+
     private Field _field;
 
     // Start is called before the first frame update
     void Start()
     {
         _field = _fieldFactory.GenerateField(_stage);
-        for (int i = 0; i < _players.Length; i++)
+        for(int i=0;i<_players.Length;i++)
         {
             (int px, int py) = _fieldFactory.GetInitialPosition(_stage, i);
-            _players[i].Setup(_field, px, py);
+            _players[i].Setup(_field, px, py, _lives, _units);
         }
     }
 
