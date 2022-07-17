74e9cd21661d3158ae461416f45ccc6520b78353
azarashin <azarashin1979@gmail.com>
2022-07-17 23:49:57 +0900

    不要なテストケースを削除

--- Assets/UnityQix/Tests/EditorTests/TesPositionUpdater.cs
+++ Assets/UnityQix/Tests/EditorTests/TesPositionUpdater.cs
@@ -788,15 +788,5 @@ public class TesPositionUpdater
 
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator TesPositionUpdaterWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
-
 
 }
